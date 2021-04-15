58c34fce2d94b1a0189d6aeca711b05761d9f3bf
fialho <danidaninpce3@gmail.com>
2021-04-15 13:20:12 -0300

    Fixing blank EditText

--- app/src/main/java/com/daniela/calculadoradenotas/MainActivity.kt
+++ app/src/main/java/com/daniela/calculadoradenotas/MainActivity.kt
@@ -24,39 +24,84 @@ class MainActivity : AppCompatActivity() {
 
         binding.btCalcular.setOnClickListener()
         {
-            var num1 = binding.nota1
-            var num2 = binding.nota2
-            var num3 = binding.nota3
-            var num4 = binding.nota4
-            var faltas = binding.faltas
-            var resultados = binding.resultado
-            var nota1 = Integer.parseInt(num1.text.toString())
-            var nota2 = Integer.parseInt(num2.text.toString())
-            var nota3 = Integer.parseInt(num3.text.toString())
-            var nota4 = Integer.parseInt(num4.text.toString())
-            var numeroFaltas = Integer.parseInt(faltas.text.toString())
-            CalculoDeNotas(nota1,nota2,nota3,nota4,numeroFaltas,resultados)
+            ValidateCamps()
         }
     }
 
-    fun CalculoDeNotas(n1:Int,n2:Int,n3:Int,n4:Int,faltas:Int, resultado: TextView)
+    fun CalculoDeNotas(num1:EditText,num2:EditText,num3:EditText,num4:EditText,faltas:EditText)
     {
-        val media = (n1 + n2 + n3 + n4)/4
-        if(media>=5 && faltas<=20)
+        var nota1 = Integer.parseInt(num1.text.toString())
+        var nota2 = Integer.parseInt(num2.text.toString())
+        var nota3 = Integer.parseInt(num3.text.toString())
+        var nota4 = Integer.parseInt(num4.text.toString())
+        var numeroFaltas = Integer.parseInt(faltas.text.toString())
+        var resultados = binding.resultado
+
+        val media = (nota1 + nota2 + nota3 + nota4)/4
+        if(media>=5 && numeroFaltas <=20)
         {
-            resultado.setText("Aluno foi aprovado. \n Média Final $media")
-            resultado.setTextColor(getColor(R.color.green))
+            resultados.setText("Aluno foi aprovado. \n Média Final $media")
+            resultados.setTextColor(getColor(R.color.green))
         }
-        else if(faltas > 20)
+        else if(numeroFaltas > 20)
         {
-            resultado.setText("Aluno foi reprovado por faltas. \n Média Final $media")
-            resultado.setTextColor(getColor(R.color.red))
+            resultados.setText("Aluno foi reprovado por faltas. \n Média Final $media")
+            resultados.setTextColor(getColor(R.color.red))
         }
         else if(media<5)
         {
-            resultado.setText("Aluno foi reprovado por nota. \n Média Final $media")
-            resultado.setTextColor(getColor(R.color.red))
+            resultados.setText("Aluno foi reprovado por nota. \n Média Final $media")
+            resultados.setTextColor(getColor(R.color.red))
         }
+
+    }
+
+    fun ValidateCamps()
+    {
+        var num1 = binding.nota1
+        var num2 = binding.nota2
+        var num3 = binding.nota3
+        var num4 = binding.nota4
+        var faltas = binding.faltas
+
+
+
+        if(num1.text.toString().isEmpty())
+        {
+
+            num1.setError("Preencha a nota 1")
+            num1.requestFocus()
+        }
+        else if(num2.text.toString().isEmpty())
+        {
+
+            num2.setError("Preencha a nota 2")
+            num2.requestFocus()
+        }
+        else if(num3.text.toString().isEmpty())
+        {
+
+            num3.setError("Preencha a nota 3")
+            num3.requestFocus()
+        }
+        else if(num4.text.toString().isEmpty())
+        {
+
+            num4.setError("Preencha a nota 4")
+            num4.requestFocus()
+        }
+        else if(faltas.text.toString().isEmpty())
+        {
+
+            faltas.setError("Preencha o número de faltas")
+            faltas.requestFocus()
+        }
+        else
+        {
+            CalculoDeNotas(num1,num2,num3,num4,faltas);
+        }
+
+
 
     }
 
